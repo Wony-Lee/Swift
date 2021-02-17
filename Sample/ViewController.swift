ee8be50352bb412ccab784726a850567f38be3e6
Wony-Lee <a1_11_11@naver.com>
2021-02-18 00:50:03 +0900

    2 Day

--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -9,18 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue = 0
+    
+    @IBOutlet weak var RandomPrice: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        RandomPrice.text = "₩\(currentValue)"
     }
 
     @IBAction func Clickme(_ sender: Any) {
-        let alert = UIAlertController(title: "Hello", message: "World", preferredStyle: .alert)
+        
+        let message = "가격은 \(currentValue)원 입니다."
+        
+        let alert = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
+        
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         
         present(alert, animated:true, completion:nil)
         
+        let randomPrice = arc4random_uniform(10000) + 1
+        currentValue = Int(randomPrice)
+        RandomPrice.text = "₩ \(currentValue)"
     }
     
 }
